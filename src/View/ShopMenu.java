101ed524f37f35ce69325ca628e30c6c2d81b0dd
sorush hatefi <123449121+Sorush-vh@users.noreply.github.com>
2023-04-14 10:37:49 -0700

    Update ShopMenu.java

--- src/View/ShopMenu.java
+++ src/View/ShopMenu.java
@@ -1,9 +1,27 @@
-package View;
+package view;
 
 import java.util.Scanner;
 
 public class ShopMenu {
+    
+
     public static void run(Scanner scanner){
 
+    };
+
+
+    public static String showItems(){
+
     }
+
+    public static String buyItemByName(String input){
+
+    }
+
+    public static String sellItemByName(String input){
+
+    }
+
+
+
 }
