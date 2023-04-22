8d9707f9afafd2b0e776a0286312f9e9f0e81111
kiarashkia138 <kiarashkia138@gmail.com>
2023-04-22 18:10:25 +0330

    make some stuff right

--- src/main/java/Model/Buildings/Building.java
+++ src/main/java/Model/Buildings/Building.java
@@ -1,7 +1,7 @@
 package Model.Buildings;
 
 import Model.Land;
-import Model.Resource;
+import Model.Resources.Resource;
 
 import java.util.ArrayList;
 
--- src/main/java/Model/Buildings/Generator.java
+++ src/main/java/Model/Buildings/Generator.java
@@ -1,6 +1,6 @@
 package Model.Buildings;
 
-import Model.Resource;
+import Model.Resources.Resource;
 
 public class Generator extends Building{
     private int rate;
