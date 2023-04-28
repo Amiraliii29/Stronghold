ef1146b249fdf1cccbbd79cf585635a36b67ea5e
kiarashkia138 <kiarashkia138@gmail.com>
2023-04-28 18:28:15 +0330

    added Engineer

---
+++ src/main/java/Model/Units/Engineer.java
@@ -0,0 +1,11 @@
+package Model.Units;
+
+import Model.Government;
+
+public class Engineer extends Unit{
+    public Engineer(Government owner, String name, int speed, int hitPoint, int damage, int attackRange, int cost) {
+        super(owner, name, speed, hitPoint, damage, attackRange, cost);
+    }
+
+    //need to be completed//TODO
+}
