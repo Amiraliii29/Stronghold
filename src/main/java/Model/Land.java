2cba236fd21f45f9a8b4b7c7415bd7fc46b9de01
Amiraliii29 <119691499+Amiraliii29@users.noreply.github.com>
2023-04-30 13:54:39 +0330

    lands enum added

--- src/main/java/Model/Land.java
+++ src/main/java/Model/Land.java
@@ -1,7 +1,21 @@
 package Model;
 
 public enum Land {
-    
+    DEFAULT("default Land"),
+    GRAVEL("gravel"),
+    FLAT_ROCK("flat rock"),
+    ROCK("rock"),
+    IRON("iron"),
+    GRASS("grass"),
+    MEADOW("meadow"),
+    FULL_MEADOW("high density meadow"),
     ;
-    String name;
+    private String name;
+
+    Land(String name) {
+        this.name = name;
+    }
+    public static String getName(Land mainEnum){
+        return mainEnum.name;
+    }
 }
