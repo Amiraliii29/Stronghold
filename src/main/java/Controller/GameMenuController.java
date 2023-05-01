0dfeb4bd92d36261d3fbbe9778af7e443211b063
kiarashkia138 <kiarashkia138@gmail.com>
2023-05-01 09:12:00 +0330

    working on move

--- src/main/java/Controller/GameMenuController.java
+++ src/main/java/Controller/GameMenuController.java
@@ -1,16 +1,18 @@
 package Controller;
 
+import Model.Buildings.Defence;
 import Model.DataBase;
 import Model.Square;
-import Model.Units.Engineer;
-import Model.Units.State;
-import Model.Units.Unit;
+import Model.Units.*;
 import View.Enums.Messages.GameMenuMessages;
+import Model.Map;
 
 import java.util.ArrayList;
 import java.util.Objects;
 
 public class GameMenuController {
+    private static ArrayList<ArrayList<Square>> allWays;
+    private static ArrayList<Square> squares;
     public static GameMenuMessages nextTurnController() {
         return null;
     }
@@ -74,22 +76,39 @@ public class GameMenuController {
 
         if (DataBase.getSelectedUnit().size() == 0) return GameMenuMessages.CHOSE_UNIT_FIRST;
 
-        int speed = DataBase.getSelectedUnit().get(0).getSpeed();
-//        int firstX = DataBase.getSelectedUnit().get(0).getXCoordinate();
-//        int firstY = DataBase.getSelectedUnit().get(0).getYCoordinate();
-//        if (Math.abs((xCoordinate - firstX)) > range || Math.abs((yCoordinate - firstY)) > range)
-//            return GameMenuMessages.OUT_OF_RANGE;
-
         if (!DataBase.getSelectedMap().getSquareFromMap(xCoordinate, yCoordinate).canPass())
             return GameMenuMessages.CANT_GO_THERE;
 
     }
 
-    private static boolean dfs(int x, int y, int xFin, int yFin, int speed, boolean up, boolean stair) {
+    private static boolean findWay(int x, int y, int xFin, int yFin, int speed, boolean up) {
         if (speed >= 0 && x == xFin && y == yFin) return true;
         if (speed == 0) return false;
 
+        Square[][] squares = DataBase.getSelectedMap().getSquares();
+        boolean stair = squares[x][y].getBuilding().getName().equals("Stair");
+        if (stair) up = !up;
+        if (!stair) {
+            if (up) {
+                if (squares[x+1][y].getBuilding() instanceof Defence &&
+                        !squares[x+1][y].getBuilding().getName().equals("DrawBridge") &&
+                        findWay(x+1, y, xFin, yFin, speed-1, up))
+                    return true;
+                else if (squares[x-1][y].getBuilding() instanceof Defence &&
+                        !squares[x-1][y].getBuilding().getName().equals("DrawBridge") &&
+                        findWay(x-1, y, xFin, yFin, speed-1, up))
+                    return true;
+                else if (squares[x][y+1].getBuilding() instanceof Defence &&
+                        !squares[x][y+1].getBuilding().getName().equals("DrawBridge") &&
+                        findWay(x, y+1, xFin, yFin, speed-1, up))
+                    return true;
+                else return squares[x][y - 1].getBuilding() instanceof Defence &&
+                            !squares[x][y - 1].getBuilding().getName().equals("DrawBridge") &&
+                            findWay(x, y - 1, xFin, yFin, speed - 1, up);
+            } else {
 
+            }
+        }
 
 //        (dfs(x+1, y, xFin, yFin, speed-1)
 //                || dfs(x-1, y, xFin, yFin, speed-1)
@@ -98,7 +117,68 @@ public class GameMenuController {
     }
 
     private static boolean moveUnit(int x, int y) {
+        ArrayList<Unit> unit = DataBase.getSelectedUnit();
 
+        int speed = unit.get(0).getSpeed();
+        int firstX = unit.get(0).getXCoordinate();
+        int firstY = unit.get(0).getYCoordinate();
+        boolean up;
+        // define up
+        squares = new ArrayList<>();
+        allWays = new ArrayList<>();
+        move(unit.get(0), DataBase.getSelectedMap(), firstX, firstY, x, y, speed, up);
+
+        //find shortest way
+        //add move left to units
+        //handle Trap if in pass //TODO
+    }
+
+    private static boolean move(Unit unit, Map map, int x, int y, int xFin, int yFin, int speed, boolean up) {
+        if (speed >= 0 && x == xFin && y == yFin) {
+            allWays.add(squares);
+            return true;
+        }
+        if (speed == 0) return false;
+
+        //conditions
+        if (!map.getSquareFromMap(x, y).canPass()) return false;
+        if (unit instanceof Siege || unit.getName().equals("Knight") || unit.getName().equals("HorseArcher")) {
+            if (!map.getSquareFromMap(x, y).getBuilding().getCanPass()
+                    || map.getSquareFromMap(x, y).getBuilding().getName().equals("Stair"))
+                return false;
+        } else if (!unit.getName().equals("Assassin")){
+            if (!(map.getSquareFromMap(x, y).getBuilding() instanceof Defence
+                    || map.getSquareFromMap(x, y).getBuilding().getCanPass()))
+                return false;
+
+            LadderMan ladderMan = LadderMan.createLadderMan(DataBase.getCurrentGovernment(), -1, -1);
+            if (map.getSquareFromMap(x, y).getBuilding().getName().equals("Stair")
+                    || map.getSquareFromMap(x, y).getUnits().contains(ladderMan)) up = !up;
+
+            if (up && !(map.getSquareFromMap(x, y).getBuilding() instanceof Defence)) return false;
+
+            if (!up && (map.getSquareFromMap(x, y).getBuilding() instanceof Defence
+                    && !map.getSquareFromMap(x, y).getBuilding().getCanPass()))
+                return false;
+        }
+
+        squares.add(map.getSquareFromMap(x+1, y));
+        move(unit, map, x+1, y, xFin, yFin, speed-1, up);
+        squares.remove(map.getSquareFromMap(x+1, y));
+
+        squares.add(map.getSquareFromMap(x-1, y));
+        move(unit, map, x-1, y, xFin, yFin, speed-1, up);
+        squares.remove(map.getSquareFromMap(x-1, y));
+
+        squares.add(map.getSquareFromMap(x, y+1));
+        move(unit, map, x, y+1, xFin, yFin, speed-1, up);
+        squares.remove(map.getSquareFromMap(x, y+1));
+
+        squares.add(map.getSquareFromMap(x, y-1));
+        move(unit, map, x, y-1, xFin, yFin, speed-1, up);
+        squares.remove(map.getSquareFromMap(x, y-1));
+
+        return allWays.size() != 0;
     }
 
     public static GameMenuMessages setUnitModeController(String option) {
