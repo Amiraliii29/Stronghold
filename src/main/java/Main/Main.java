1cc6d9ca5b1e75f1b8034cc6cc9f848347183943
kiarashkia138 <kiarashkia138@gmail.com>
2023-06-28 16:27:56 +0330

    main updated

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -8,13 +8,25 @@ import View.SignUpMenu;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-public class Main extends Application {
-    public static void main(String[] args) throws FileNotFoundException, IOException, NoSuchAlgorithmException {
-        launch(args);
-    }
 
-    @Override
-    public void start(Stage stage) throws Exception {
-        new SignUpMenu().start(stage);
+public class Main {
+    public static void main(String[] args) {
+        try {
+            System.out.println("started !");
+            new Client("localhost", 8000);
+        } catch (IOException ignored) {
+        }
     }
 }
+
+
+//public class Main extends Application {
+//    public static void main(String[] args) throws FileNotFoundException, IOException, NoSuchAlgorithmException {
+//        launch(args);
+//    }
+//
+//    @Override
+//    public void start(Stage stage) throws Exception {
+//        new SignUpMenu().start(stage);
+//    }
+//}
