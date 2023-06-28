4df72698c3062389757a64adbbf83b71394bd7d9
kiarashkia138 <kiarashkia138@gmail.com>
2023-06-28 13:13:50 +0330

    added files

---
+++ src/main/java/Client.java
@@ -0,0 +1,17 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class Client {
+    final DataInputStream dataInputStream;
+    final DataOutputStream dataOutputStream;
+
+    public Client(String host, int port) throws IOException {
+        Socket socket = new Socket(host, port);
+        dataInputStream = new DataInputStream(socket.getInputStream());
+        dataOutputStream = new DataOutputStream(socket.getOutputStream());
+
+        Request.setUserToken(dataInputStream.readUTF());
+    }
+}
---
+++ src/main/java/Request.java
@@ -0,0 +1,40 @@
+import com.google.gson.Gson;
+
+import java.util.HashMap;
+
+public class Request {
+    private static String userToken;
+    private final String token;
+    public GameRequest gameRequest;
+    public NormalRequest normalRequest;
+    public HashMap<String, String> argument;
+
+
+
+    public static void setUserToken(String userToken) {
+        Request.userToken = userToken;
+    }
+
+
+
+    public Request(GameRequest gameRequest, NormalRequest normalRequest) {
+        token = userToken;
+        this.gameRequest = gameRequest;
+        this.normalRequest = normalRequest;
+        argument = new HashMap<>();
+    }
+
+
+
+    public boolean verify(String token) {
+        return this.token.equals(token);
+    }
+
+    public String toJson() {
+        return new Gson().toJson(this);
+    }
+
+    public static Request fromJson(String json) {
+        return new Gson().fromJson(json, Request.class);
+    }
+}
